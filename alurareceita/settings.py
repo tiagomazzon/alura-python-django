bd44bfd0e709cfa15dcabf7973d817eee6826a11
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-11-03 09:03:06 -0300

    installed postgres module; added db config; new class/model Receita

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita',
+        'USER': 'postgres',
+        'PASSWORD':'A12345678a',
+        'HOST': 'localhost'
     }
 }
 
