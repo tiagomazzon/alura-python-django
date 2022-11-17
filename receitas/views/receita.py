ddfca348ca6927950a82eebe36a4b5e4c4d5891f
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-11-17 11:10:07 -0300

    feat: refactoring recipe search

--- receitas/views/receita.py
+++ receitas/views/receita.py
@@ -24,19 +24,6 @@ def receita(request, receita_id):
     }
     return render(request,'receitas/receita.html', receita_a_exibir)
 
-def busca(request):
-    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
-
-    if 'buscar' in request.GET:
-        nome_a_buscar = request.GET['buscar']
-        if nome_a_buscar:
-            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
-
-    dados = {
-        'receitas': lista_receitas
-    }
-    return render(request, 'receitas/resultados.html', dados)
-
 def cria_receita(request):
 
     if request.method == 'POST':
