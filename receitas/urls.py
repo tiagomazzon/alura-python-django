6b81da44a3eb11e4efe0f7afeb4a3350e45ee347
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-11-17 09:01:54 -0300

    feat: refactoring templates and views folders

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:receita_id>', views.receita, name='receita'),
-    path('resultadobusca', views.resultados, name='resultadobusca')
+    path('', index, name='index'),
+    path('<int:receita_id>', receita, name='receita'),
+    path('resultadobusca', busca, name='resultadobusca'),
+    path('cria/receita', cria_receita, name='cria_receita'),
+    path('edita/<int:receita_id>', edita_receita, name='edita_receita'),
+    path('deleta/<int:receita_id>', deleta_receita, name='deleta_receita'),
+    path('atualiza_receita', atualiza_receita, name='atualiza_receita')
 ]
